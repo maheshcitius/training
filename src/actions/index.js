fbb0b1b880c0eab88f9056892829ac4c8ded738d
PournimaCTech <Pournima.Padelkar@citiustech.com>
2021-10-26 10:41:35 +0530

    index file changes after expecting the pull changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,9 @@
 export * as snackbarActions from './snackbar-actions.js'
 export * from './user.action';
-<<<<<<< HEAD
 export * as FormDialogsAction from './FormDialogs-action.js';
 export * from './medicaldata.action';
 export * from './appointments.actions';
 export * from './physicians.actions';
 export * from './patient.action';
-=======
-export * from './physicians.actions';
 export * from './demographics.actions.js'
-export * from './medicaldata.action'
 export * from './appointments.actions'
->>>>>>> eb3b0dce6bd85124ea234dd7d9c5178a58b1dda9
